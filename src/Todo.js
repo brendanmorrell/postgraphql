ffb854ca6b4ffbc2ad28aa23759469767f444e9a
Brendan Morrell <brendanmorrell@gmail.com>
2019-03-22 13:53:02 -0700

    got prettier and styled components and the xicon hover wired up

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-export default ({ id, task, completed }) => (
-  <div>
-    {id}:{task}, Completed: {String(completed)}
-  </div>
-)
+const XIcon = styled.span`
+  width: 110px;
+  height: 110px;
+  border-radius: 50%;
+  border: 1px solid black;
+  cursor: pointer;
+`;
+class Todo extends Component {
+  state = { hovering: false };
+  render() {
+    const { id, task, completed, onClick } = this.props;
+    const { hovering } = this.state;
+    return (
+      <div
+        onMouseEnter={() => this.setState({ hovering: true })}
+        onMouseLeave={() => this.setState({ hovering: false })}
+      >
+        # {id} : {task} <input type="checkbox" checked={completed} />{' '}
+        {hovering && <XIcon onClick={onClick}>X</XIcon>}
+      </div>
+    );
+  }
+}
+export default props => <Todo {...props} />;
